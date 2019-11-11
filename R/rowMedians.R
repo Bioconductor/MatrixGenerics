c3da5777da8d966a9316d443d8fd4142a9e623d7
const-ae <artjom31415@googlemail.com>
2019-11-11 11:12:23 +0100

    Add rows and cols back to the generic signature

--- R/rowMedians.R
+++ R/rowMedians.R
@@ -3,6 +3,9 @@
 #' Calculates the median for each row (column) of a matrix-like object.
 #'
 #' @param x An NxK matrix-like object.
+#' @param rows,cols A \code{\link[base]{vector}} indicating subset of rows
+#' (and/or columns) to operate over. If \code{\link[base]{NULL}}, no subsetting
+#' is done.
 #' @param na.rm If \code{\link[base:logical]{TRUE}}, \code{\link[base]{NA}}s
 #' are excluded first, otherwise not.
 #' @param ... Additional arguments passed to specific methods.
@@ -25,12 +28,14 @@
 #'
 #' @name rowMedians
 #' @export
-setGeneric("rowMedians", function(x, na.rm=FALSE, ...) standardGeneric("rowMedians"),
+setGeneric("rowMedians", function(x, rows = NULL, cols = NULL, na.rm=FALSE, ...) standardGeneric("rowMedians"),
            signature = "x",
 )
 
-setMethod("rowMedians", signature = "matrix", function(x, na.rm=FALSE, ...){
-  matrixStats::rowMedians(x, na.rm=na.rm, ...)
+#' @rdname rowMedians
+setMethod("rowMedians", signature = "matrix", function(x, rows = NULL, cols = NULL, na.rm=FALSE, ...){
+  matrixStats::rowMedians(x, rows = rows, cols = cols, na.rm=na.rm, ...)
+})
 })
 
 
@@ -38,12 +43,13 @@ setMethod("rowMedians", signature = "matrix", function(x, na.rm=FALSE, ...){
 
 #' @rdname rowMedians
 #' @name colMedians
-setGeneric("colMedians", function(x, na.rm=FALSE, ...) standardGeneric("colMedians"),
+setGeneric("colMedians", function(x, rows = NULL, cols = NULL, na.rm=FALSE, ...) standardGeneric("colMedians"),
            signature = "x",
 )
 
-setMethod("colMedians", signature = "matrix", function(x, na.rm=FALSE, ...){
-  matrixStats::colMedians(x, na.rm=na.rm, ...)
+#' @rdname rowMedians
+setMethod("colMedians", signature = "matrix", function(x, rows = NULL, cols = NULL, na.rm=FALSE, ...){
+  matrixStats::colMedians(x, rows = rows, cols = cols, na.rm=na.rm, ...)
 })
 
 
