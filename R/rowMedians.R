8e7bf40aa99355cd7431f5661667beb01fc61125
const-ae <artjom31415@googlemail.com>
2019-11-25 10:28:22 +0100

    Remove rows and cols again from the generic
    See discussion at https://github.com/Bioconductor/MatrixGenerics/pull/5#issuecomment-556162347

--- R/rowMedians.R
+++ R/rowMedians.R
@@ -3,9 +3,6 @@
 #' Calculates the median for each row (column) of a matrix-like object.
 #'
 #' @param x An NxK matrix-like object.
-#' @param rows,cols A \code{\link[base]{vector}} indicating subset of rows
-#' (and/or columns) to operate over. If \code{\link[base]{NULL}}, no subsetting
-#' is done.
 #' @param na.rm If \code{\link[base:logical]{TRUE}}, \code{\link[base]{NA}}s
 #' are excluded first, otherwise not.
 #' @param dim. An \code{\link[base]{integer}} \code{\link[base]{vector}} of
@@ -31,18 +28,18 @@
 #'
 #' @name rowMedians
 #' @export
-setGeneric("rowMedians", function(x, rows = NULL, cols = NULL, na.rm=FALSE, ...) standardGeneric("rowMedians"),
+setGeneric("rowMedians", function(x, na.rm=FALSE, ...) standardGeneric("rowMedians"),
            signature = "x",
 )
 
 #' @rdname rowMedians
-setMethod("rowMedians", signature = "matrix", function(x, rows = NULL, cols = NULL, na.rm=FALSE, ...){
-  matrixStats::rowMedians(x, rows = rows, cols = cols, na.rm=na.rm, ...)
+setMethod("rowMedians", signature = "matrix", function(x, na.rm=FALSE, ...){
+  matrixStats::rowMedians(x, na.rm=na.rm, ...)
 })
 
 #' @rdname rowMedians
-setMethod("rowMedians", signature = "numeric", function(x, rows = NULL, cols = NULL, na.rm=FALSE, dim., ...){
-  matrixStats::rowMedians(x, rows = rows, cols = cols, na.rm=na.rm, dim.=dim., ...)
+setMethod("rowMedians", signature = "numeric", function(x, na.rm=FALSE, dim., ...){
+  matrixStats::rowMedians(x, na.rm=na.rm, dim.=dim., ...)
 })
 
 
@@ -50,18 +47,18 @@ setMethod("rowMedians", signature = "numeric", function(x, rows = NULL, cols = N
 
 #' @rdname rowMedians
 #' @name colMedians
-setGeneric("colMedians", function(x, rows = NULL, cols = NULL, na.rm=FALSE, ...) standardGeneric("colMedians"),
+setGeneric("colMedians", function(x, na.rm=FALSE, ...) standardGeneric("colMedians"),
            signature = "x",
 )
 
 #' @rdname rowMedians
-setMethod("colMedians", signature = "matrix", function(x, rows = NULL, cols = NULL, na.rm=FALSE, ...){
-  matrixStats::colMedians(x, rows = rows, cols = cols, na.rm=na.rm, ...)
+setMethod("colMedians", signature = "matrix", function(x, na.rm=FALSE, ...){
+  matrixStats::colMedians(x, na.rm=na.rm, ...)
 })
 
 #' @rdname rowMedians
-setMethod("colMedians", signature = "numeric", function(x, rows = NULL, cols = NULL, na.rm=FALSE, dim., ...){
-  matrixStats::colMedians(x, rows = rows, cols = cols, na.rm=na.rm, dim. = dim., ...)
+setMethod("colMedians", signature = "numeric", function(x, na.rm=FALSE, dim., ...){
+  matrixStats::colMedians(x, na.rm=na.rm, dim. = dim., ...)
 })
 
 
