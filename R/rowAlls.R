af44dfac4d26e5a05d1dba048c0e4109845ace72
const-ae <artjom31415@googlemail.com>
2020-02-29 15:06:40 +0100

    Implement rowAlls, rowAnys, and rowAnyNAs

---
+++ R/rowAlls.R
@@ -0,0 +1,70 @@
+#' Check if all elements in a row (column) of a matrix-like object are equal to a value
+#'
+#' Check if all elements in a row (column) of a matrix-like object are equal to a value
+#' 
+#' @templateVar rowName rowAlls
+#' @templateVar colName colAlls
+#' 
+#' @template matrixStatsLink
+#' 
+#' @template standardParameters
+#' @template valueParameter
+#' @template na_rmParameter
+#'
+#' @template returnVectorLgl
+#'
+#' @seealso
+#' \itemize{
+#' \item \code{matrixStats::\link[matrixStats]{rowAlls}()} and
+#'   \code{matrixStats::\link[matrixStats]{colAlls}()} which are used when
+#'   the input is a \code{matrix} or \code{numeric} vector.
+#' \item For checks if any element is equal to a value, see \code{\link{rowAnys}()}.
+#' }
+#' 
+#' @template standardExamples
+#'
+#'
+#' @name rowAlls
+#' @export
+setGeneric("rowAlls", function(x, rows = NULL, cols = NULL, value = TRUE, na.rm=FALSE, ...) standardGeneric("rowAlls"),
+           signature = "x"
+)
+
+.default_rowAlls <- function(x, rows = NULL, cols = NULL, value = TRUE, na.rm=FALSE, dim. = dim(x)){
+  matrixStats::rowAlls(x, rows = rows, cols = cols, value = value, na.rm=na.rm, dim. = dim.)
+}
+
+#' @rdname rowAlls
+setMethod("rowAlls", signature = "matrix", .default_rowAlls)
+
+#' @rdname rowAlls
+setMethod("rowAlls", signature = "numeric", .default_rowAlls)
+
+#' @rdname rowAlls
+setMethod("rowAlls", signature = "array", .default_rowAlls)
+
+
+
+
+#' @rdname rowAlls
+#' @name colAlls
+#' @export
+setGeneric("colAlls", function(x, rows = NULL, cols = NULL, value = TRUE, na.rm=FALSE, ...) standardGeneric("colAlls"),
+           signature = "x"
+)
+
+.default_colAlls <- function(x, rows = NULL, cols = NULL, value = TRUE, na.rm=FALSE, dim. = dim(x)){
+  matrixStats::colAlls(x, rows = rows, cols = cols, value = value, na.rm=na.rm, dim. = dim.)
+}
+
+#' @rdname rowAlls
+setMethod("colAlls", signature = "matrix", .default_colAlls)
+
+#' @rdname rowAlls
+setMethod("colAlls", signature = "numeric", .default_colAlls)
+
+#' @rdname rowAlls
+setMethod("colAlls", signature = "array", .default_colAlls)
+
+
+
---
+++ R/rowAnyNAs.R
@@ -0,0 +1,69 @@
+#' Check if any elements in a row (column) of a matrix-like object is missing
+#'
+#' Check if any elements in a row (column) of a matrix-like object is missing
+#' 
+#' @templateVar rowName rowAnyNAs
+#' @templateVar colName colAnyNAs
+#' 
+#' @template matrixStatsLink
+#' 
+#' @template standardParameters
+#' @template na_rmParameter
+#'
+#' @template returnVectorLgl
+#'
+#' @seealso
+#' \itemize{
+#' \item \code{matrixStats::\link[matrixStats]{rowAnyNAs}()} and
+#'   \code{matrixStats::\link[matrixStats]{colAnyNAs}()} which are used when
+#'   the input is a \code{matrix} or \code{numeric} vector.
+#' \item For checks if any element is equal to a value, see \code{\link{rowAnyNAs}()}.
+#' }
+#' 
+#' @template standardExamples
+#'
+#'
+#' @name rowAnyNAs
+#' @export
+setGeneric("rowAnyNAs", function(x, rows = NULL, cols = NULL, na.rm=FALSE, ...) standardGeneric("rowAnyNAs"),
+           signature = "x"
+)
+
+.default_rowAnyNAs <- function(x, rows = NULL, cols = NULL, na.rm=FALSE, dim. = dim(x)){
+  matrixStats::rowAnyNAs(x, rows = rows, cols = cols, na.rm=na.rm, dim. = dim.)
+}
+
+#' @rdname rowAnyNAs
+setMethod("rowAnyNAs", signature = "matrix", .default_rowAnyNAs)
+
+#' @rdname rowAnyNAs
+setMethod("rowAnyNAs", signature = "numeric", .default_rowAnyNAs)
+
+#' @rdname rowAnyNAs
+setMethod("rowAnyNAs", signature = "array", .default_rowAnyNAs)
+
+
+
+
+#' @rdname rowAnyNAs
+#' @name colAnyNAs
+#' @export
+setGeneric("colAnyNAs", function(x, rows = NULL, cols = NULL, na.rm=FALSE, ...) standardGeneric("colAnyNAs"),
+           signature = "x"
+)
+
+.default_colAnyNAs <- function(x, rows = NULL, cols = NULL, na.rm=FALSE, dim. = dim(x)){
+  matrixStats::colAnyNAs(x, rows = rows, cols = cols, na.rm=na.rm, dim. = dim.)
+}
+
+#' @rdname rowAnyNAs
+setMethod("colAnyNAs", signature = "matrix", .default_colAnyNAs)
+
+#' @rdname rowAnyNAs
+setMethod("colAnyNAs", signature = "numeric", .default_colAnyNAs)
+
+#' @rdname rowAnyNAs
+setMethod("colAnyNAs", signature = "array", .default_colAnyNAs)
+
+
+
