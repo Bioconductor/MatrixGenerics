dd9a881161a7edc086d43011048a68e3f801cf04
const-ae <artjom31415@googlemail.com>
2020-02-29 15:44:15 +0100

    Implement rowCumprods / rowCumsums

---
+++ R/rowCumprods.R
@@ -0,0 +1,68 @@
+#' Calculates the cumulative maxima for each row (column) of a matrix-like object
+#'
+#' Calculates the cumulative maxima for each row (column) of a matrix-like object
+#' 
+#' @templateVar rowName rowCumprods
+#' @templateVar colName colCumprods
+#' 
+#' @template matrixStatsLink
+#' 
+#' @template standardParameters
+#'
+#' @template returnMatrix_SameDimX
+#'
+#' @seealso
+#' \itemize{
+#' \item \code{matrixStats::\link[matrixStats]{rowCumprods}()} and
+#'   \code{matrixStats::\link[matrixStats]{colCumprods}()} which are used when
+#'   the input is a \code{matrix} or \code{numeric} vector.
+#' }
+#' 
+#' @template standardExamples
+#'
+#' @keywords array iteration robust univar
+#'
+#' @name rowCumprods
+#' @export
+setGeneric("rowCumprods", function(x, rows = NULL, cols = NULL,  ...) standardGeneric("rowCumprods"),
+           signature = "x"
+)
+
+.default_rowCumprods <- function(x, rows = NULL, cols = NULL, dim. = dim(x)){
+  matrixStats::rowCumprods(x, rows = rows, cols = cols, dim. = dim.)
+}
+
+#' @rdname rowCumprods
+setMethod("rowCumprods", signature = "matrix", .default_rowCumprods)
+
+#' @rdname rowCumprods
+setMethod("rowCumprods", signature = "numeric", .default_rowCumprods)
+
+#' @rdname rowCumprods
+setMethod("rowCumprods", signature = "array", .default_rowCumprods)
+
+
+
+
+#' @rdname rowCumprods
+#' @name colCumprods
+#' @export
+setGeneric("colCumprods", function(x, rows = NULL, cols = NULL, ...) standardGeneric("colCumprods"),
+           signature = "x"
+)
+
+.default_colCumprods <- function(x, rows = NULL, cols = NULL, dim. = dim(x)){
+  matrixStats::colCumprods(x, rows = rows, cols = cols, dim. = dim.)
+}
+
+#' @rdname rowCumprods
+setMethod("colCumprods", signature = "matrix", .default_colCumprods)
+
+#' @rdname rowCumprods
+setMethod("colCumprods", signature = "numeric", .default_colCumprods)
+
+#' @rdname rowCumprods
+setMethod("colCumprods", signature = "array", .default_colCumprods)
+
+
+
---
+++ R/rowCumsums.R
@@ -0,0 +1,68 @@
+#' Calculates the cumulative maxima for each row (column) of a matrix-like object
+#'
+#' Calculates the cumulative maxima for each row (column) of a matrix-like object
+#' 
+#' @templateVar rowName rowCumsums
+#' @templateVar colName colCumsums
+#' 
+#' @template matrixStatsLink
+#' 
+#' @template standardParameters
+#'
+#' @template returnMatrix_SameDimX
+#'
+#' @seealso
+#' \itemize{
+#' \item \code{matrixStats::\link[matrixStats]{rowCumsums}()} and
+#'   \code{matrixStats::\link[matrixStats]{colCumsums}()} which are used when
+#'   the input is a \code{matrix} or \code{numeric} vector.
+#' }
+#' 
+#' @template standardExamples
+#'
+#' @keywords array iteration robust univar
+#'
+#' @name rowCumsums
+#' @export
+setGeneric("rowCumsums", function(x, rows = NULL, cols = NULL,  ...) standardGeneric("rowCumsums"),
+           signature = "x"
+)
+
+.default_rowCumsums <- function(x, rows = NULL, cols = NULL, dim. = dim(x)){
+  matrixStats::rowCumsums(x, rows = rows, cols = cols, dim. = dim.)
+}
+
+#' @rdname rowCumsums
+setMethod("rowCumsums", signature = "matrix", .default_rowCumsums)
+
+#' @rdname rowCumsums
+setMethod("rowCumsums", signature = "numeric", .default_rowCumsums)
+
+#' @rdname rowCumsums
+setMethod("rowCumsums", signature = "array", .default_rowCumsums)
+
+
+
+
+#' @rdname rowCumsums
+#' @name colCumsums
+#' @export
+setGeneric("colCumsums", function(x, rows = NULL, cols = NULL, ...) standardGeneric("colCumsums"),
+           signature = "x"
+)
+
+.default_colCumsums <- function(x, rows = NULL, cols = NULL, dim. = dim(x)){
+  matrixStats::colCumsums(x, rows = rows, cols = cols, dim. = dim.)
+}
+
+#' @rdname rowCumsums
+setMethod("colCumsums", signature = "matrix", .default_colCumsums)
+
+#' @rdname rowCumsums
+setMethod("colCumsums", signature = "numeric", .default_colCumsums)
+
+#' @rdname rowCumsums
+setMethod("colCumsums", signature = "array", .default_colCumsums)
+
+
+
