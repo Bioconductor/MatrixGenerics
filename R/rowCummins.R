36bc49ebc622d2764c67007986b10cbfd5ced7c1
const-ae <artjom31415@googlemail.com>
2020-02-29 15:40:52 +0100

    Implement rowCummins/rowCummaxs

---
+++ R/rowCummins.R
@@ -0,0 +1,69 @@
+#' Calculates the cumulative minima for each row (column) of a matrix-like object
+#'
+#' Calculates the cumulative minima for each row (column) of a matrix-like object
+#' 
+#' @templateVar rowName rowCummins
+#' @templateVar colName colCummins
+#' 
+#' @template matrixStatsLink
+#' 
+#' @template standardParameters
+#'
+#' @template returnMatrix_SameDimX
+#'
+#' @seealso
+#' \itemize{
+#' \item \code{matrixStats::\link[matrixStats]{rowCummins}()} and
+#'   \code{matrixStats::\link[matrixStats]{colCummins}()} which are used when
+#'   the input is a \code{matrix} or \code{numeric} vector.
+#' \item For single minimum estimates, see \code{\link{rowMins}()}.
+#' }
+#' 
+#' @template standardExamples
+#'
+#' @keywords array iteration robust univar
+#'
+#' @name rowCummins
+#' @export
+setGeneric("rowCummins", function(x, rows = NULL, cols = NULL,  ...) standardGeneric("rowCummins"),
+           signature = "x"
+)
+
+.default_rowCummins <- function(x, rows = NULL, cols = NULL, dim. = dim(x)){
+  matrixStats::rowCummins(x, rows = rows, cols = cols, dim. = dim.)
+}
+
+#' @rdname rowCummins
+setMethod("rowCummins", signature = "matrix", .default_rowCummins)
+
+#' @rdname rowCummins
+setMethod("rowCummins", signature = "numeric", .default_rowCummins)
+
+#' @rdname rowCummins
+setMethod("rowCummins", signature = "array", .default_rowCummins)
+
+
+
+
+#' @rdname rowCummins
+#' @name colCummins
+#' @export
+setGeneric("colCummins", function(x, rows = NULL, cols = NULL, ...) standardGeneric("colCummins"),
+           signature = "x"
+)
+
+.default_colCummins <- function(x, rows = NULL, cols = NULL, dim. = dim(x)){
+  matrixStats::colCummins(x, rows = rows, cols = cols, dim. = dim.)
+}
+
+#' @rdname rowCummins
+setMethod("colCummins", signature = "matrix", .default_colCummins)
+
+#' @rdname rowCummins
+setMethod("colCummins", signature = "numeric", .default_colCummins)
+
+#' @rdname rowCummins
+setMethod("colCummins", signature = "array", .default_colCummins)
+
+
+
---
+++ man-roxygen/returnMatrix_SameDimX.R
@@ -0,0 +1,3 @@
+#' @md
+#' @return Returns a \code{\link[base]{numeric}} \code{\link[base]{matrix}} with the 
+#'   same dimensions as \code{x}.
