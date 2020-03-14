fb16f5bd43e606e6ddc7fbc2e1c188c7f6b5669e
const-ae <artjom31415@googlemail.com>
2020-03-14 14:41:46 +0100

    Use template for return in rowQuantiles

---
+++ man-roxygen/returnMatrix_JDim.R
@@ -0,0 +1,3 @@
+#' @md
+#' @return a \code{\link[base]{numeric}} \code{NxJ} (\code{KxJ}) \code{\link{matrix}}, where
+#'   N (K) is the number of rows (columns) for which the J values are calculated. 
