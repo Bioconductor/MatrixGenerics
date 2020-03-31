5de92b56b89846b0d52d0da9f1848d7bd3b629f2
PeteHaitch <peter.hickey@gmail.com>
2020-03-31 14:26:45 +1100

    Import a single function to quieten R CMD check NOTE:
    Package in Depends field not imported from: ‘matrixStats’
        These packages need to be imported from (in the NAMESPACE file)
        for when this namespace is loaded but not attached.

--- R/MatrixGenerics-package.R
+++ R/MatrixGenerics-package.R
@@ -1,3 +1,7 @@
-
+# NOTE: Import a single function to quieten R CMD check NOTE:
+#         Package in Depends field not imported from: ‘matrixStats’
+#           These packages need to be imported from (in the NAMESPACE file)
+#           for when this namespace is loaded but not attached.
+#' @importFrom matrixStats allocArray
 #' @import methods
 NULL
